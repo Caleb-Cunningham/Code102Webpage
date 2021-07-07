c3a089409087d66721f3e8b69d0523fea047ca96
Caleb Ethan Cunningham <ethan.4than84@gmail.com>
2021-07-07 18:18:22 -0500

    Fixed the loop

--- scripts.js
+++ scripts.js
@@ -3,13 +3,17 @@ let password = prompt("Enter website passcode");
 while(password != "1307"){
     for(let i = 0; i < 5; i++){
         password = prompt("Passcode enter failed");
-
+        if(password == "1307"){
+          break;
+        }
     }
+
     alert("Code to acces webiste is 1307");
   }
 
 }
 
 getPassword();
+
 
 
